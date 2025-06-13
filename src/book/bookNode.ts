507b10559c248c260daa62db775f1eab9cfd27bb
Alifais Farrel Ramdhani <farrellabindonesia@gmail.com>
2025-06-13 23:13:42 +0700

    Update bookNode.ts

--- src/book/bookNode.ts
+++ src/book/bookNode.ts
@@ -1,16 +1,15 @@
-// Book node for Readmap AI
 export interface bookNode {
-    isbn13 : number,
-    isbn10 : number,
-    title : String,
-    subtitle : String,
-    author : String,
-    categories : String,
-    thumbail : String, // link to the picture
-    description : String,
-    published_year : number,
-    average_rating : number,
-    num_page : number,
-    embedding: number[], // array of numbers with length 3072
-    prevNodes : bookNode[] // array of previous nodes
+  id: string
+  isbn13: number
+  isbn10: string
+  title: string
+  subtitle: string
+  author: string
+  categories: string
+  thumbnail: string
+  description: string
+  published_year: number
+  average_rating: number
+  num_pages: number
+  prerequisites: number[] //isbn as the prerequisites
 }
